ef3517f37bbc90ad74f040fca39b89d2660230ca
sashakil074 <shakil.ahammed074@gmail.com>
2022-09-25 08:12:13 +0600

    updated

--- bresenham_line_draw.cpp
+++ bresenham_line_draw.cpp
@@ -1,108 +1,131 @@
-//bresenham Line draw
+//bresenham line draw
 #include<bits/stdc++.h>
 #include<graphics.h>
+
 using namespace std;
 
 void plotgraph(int screenWidth,int screenHeight)
 {
-	//plotting grids
-	for(int i=screenWidth/2;i<=screenWidth;i=i+25)
+	int i,j;
+	
+	for(i=screenWidth/2;i<=screenWidth;i=i+10)
 	{
-		for(int j=screenHeight/2;j<=screenHeight;j=j+25)
+		for(j=screenHeight/2;j<=screenHeight;j=j+10)
 		{
-			setcolor(WHITE);
-			line(0,j,screenWidth,j);
+			setcolor(MAGENTA);
 			line(i,0,i,screenHeight);
+			line(0,j,screenWidth,j);
 		}
 	}
-	for(int i=screenWidth/2;i>=0;i=i-25)
+		for(i=screenWidth/2;i>=0;i=i-10)
 	{
-		for(int j=screenHeight/2;j>=0;j=j-25)
+		for(j=screenHeight/2;j>=0;j=j-10)
 		{
-			setcolor(WHITE);
-			line(0,j,screenWidth,j);
+			setcolor(MAGENTA);
 			line(i,0,i,screenHeight);
+			line(0,j,screenWidth,j);
 		}
 	}
-	//plotting origin axes
+	
 	setcolor(YELLOW);
 	line(0,screenHeight/2,screenWidth,screenHeight/2);
 	line(screenWidth/2,0,screenWidth/2,screenHeight);
 }
-int main(){
+int main()
+{
+	int gd=DETECT,gm,i;
 	
-	float x,y,x0,y0,x1,y1,dx,dy,pk;
+	int screenWidth=GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight=GetSystemMetrics(SM_CYSCREEN);
 	
-	//graphics driver
-	int gd=DETECT,gm,step;
-	initgraph(&gd,&gm,"c:\\tc\\bgi");
-	
-	//window size measurement and initialization
-	DWORD screenWidth=GetSystemMetrics(SM_CXSCREEN);
-	DWORD screenHeight=GetSystemMetrics(SM_CYSCREEN);
-	initwindow(screenWidth,screenHeight,"",-3,-3);
-	
-	//graph plotting function call
+	initwindow(screenWidth,screenHeight,"window",5,5);
 	plotgraph(screenWidth,screenHeight);
 	
+	float x,y,x0,y0,x1,y1,dx,dy,xInc,yInc,p=0;
+	int step;
 	
-	cout<<"Enter the starting point (x0,y0): ";
+	cout<<"Enter the starting point: (x0,y0): ";
 	cin>>x0>>y0;
-	cout<<"Enter the ending point (x1,y1): ";
+	cout<<"Enter the ending point: (x1,y1): ";
 	cin>>x1>>y1;
 	
 	dx=abs(x1-x0);
 	dy=abs(y1-y0);
 	
+	if((y1-y0)/(x1-x0)<1){
 	
-
-	if(x0>x1)
-	  {
-	  	x=x1;
-	  	y=y1;
-	  	step=x0;
-	  }
-	else
-	{
+	
+	if(x0<x1){
 		x=x0;
 		y=y0;
 		step=x1;
 	}
-	
-//	x = (dy/dx > 1) ? y : x;
-//	y = (dy/dx > 1) ? x : y;
-	
-	pk=2*dy-dx;
-	
-//	cout<<"x\ty"<<endl;	
+	else
+	{
+		x=x1;
+		y=y1;
+		step=x0;
+	}
     
-    for(int i=x;i<step;i++)
-    {
-    //	cout<<(int)(x)<<"\t"<<(int)(y)<<endl;
-    //	cout<<(screenWidth/2)+((int) (x))<<"\t"<<(screenWidth/2)-((int)(y))<<endl;
-        putpixel((screenWidth/2)+x,(screenHeight/2)-y,GREEN);
-        
-        if(pk<0)
-        {
-        	
-        	y=y;
-        	pk=pk+2*dy;
+    p=2*dy-dx;
+	for(int i=x;i<step;i++)
+	{
+	//	setlinestyle(0,0,3);
+		putpixel((screenWidth)/2+x,(screenHeight)/2-y,YELLOW);
+		
+		if(p<0)
+		{
+			y=y;
+			p=p+2*dy;
 		}
 		else
 		{
-        	y=y+1;
-        	pk=pk+2*dy-2*dx;
+			y=y+1;
+			p=p+2*dy-2*dx;
 		}
-        x=x+1;
-        outtextxy((screenWidth/2)+x0,(screenHeight/2)-y0+10, "Starting point");
-        outtextxy((screenWidth/2)+x1+10,(screenHeight/2)-y1-10, "Ending point");
-        outtextxy((screenWidth/2)+5,(screenHeight/2)+5, "(0,0)");
-
-    	
+		x=x+1;
+		
+	}
+	outtextxy((screenWidth)/2+x+5,(screenHeight)/2-y-5,"Ending point");
+	outtextxy((screenWidth)/2+x0+5,(screenHeight)/2-y0-5,"start point");
+   }
+   else
+  {
+		
+	if(x0<x1){
+		x=x0;
+		y=y0;
+		step=y1;
+	}
+	else
+	{
+		x=x1;
+		y=y1;
+		step=y0;
 	}
     
-	getch();
-	closegraph();
-	
+    p=2*dx-dy;
+	for(int i=y;i<step;i++)
+	{
+		//setlinestyle(0,0,3);
+		putpixel((screenWidth)/2+x,(screenHeight)/2-y,YELLOW);
+		
+		if(p<0)
+		{
+			x=x;
+			p=p+2*dx;
+		}
+		else
+		{
+			x=x+1;
+			p=p+2*dx-2*dy;
+		}
+		y=y+1;
+		
+	}
+	outtextxy((screenWidth)/2+x+5,(screenHeight)/2-y-5,"Ending point");
+	outtextxy((screenWidth)/2+x0+5,(screenHeight)/2-y0-5,"start point");
+   }
 
+	getch();
 }
